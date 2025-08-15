334ea7fcd83c0e0bf532ef206d4c848e9c1be15a
Rehan <dhanujarehan@gmail.com>
2025-08-16 00:39:51 +0530

    files created and firts two funtions done

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+
+class Dashboard:
+    
+    def __init__(self):
+        if "data" not in st.session_state:
+            st.session_state.data = []
+    
+    def add_entry(self,name,score,catagory):
+        if name and score is not None:
+            st.session_state.data.append({"Name":name, "Score": score})
+            return True
+        return False
+   
+        
