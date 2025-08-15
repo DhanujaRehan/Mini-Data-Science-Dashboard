cb759bd62f6171274f79a7c65b8aa6b68c2511e9
Rehan <dhanujarehan@gmail.com>
2025-08-16 01:05:05 +0530

    save to excel, plot, and clear data functions have done

--- main.py
+++ main.py
@@ -8,13 +8,36 @@ class Dashboard:
         if "data" not in st.session_state:
             st.session_state.data = []
     
-    def add_entry(self,name,score,catagory):
+    def add_entry(self,name,score,category):
         if name and score is not None:
-            st.session_state.data.append({"Name":name, "Score": score})
+            st.session_state.data.append({"Name": name, "Score": score, "Category": category})
             return True
         return False
    
     def get_dataframe(self):
         return pd.DataFrame(st.session_state.data)
+
+    def save_to_excel(self,filename = "MiniDashBoard.xlsx"):
+        df = self.get_dataframe()
+        if not df.empty:
+            df.to_excel(filename, index=False)
+            return filename
+        return None
+    
+    def plot_data(self):
+        df = self.get_dataframe()
+        if not df.empty:
+            fig,ax = plt.subplots()
+            ax.bar(df["Name"],df["Score"],df["category"],color="blue")
+            plt.xticks(rotation = 45)
+            ax.set_title("Score Chart")
+            ax.set_xlabel("Name")
+            ax.set_ylabel("Score")
+            st.pyplot(fig)
+    def clear_data(self):
+        st.session_state.data = []
+    
+     
+     
     
     
